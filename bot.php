fdb44faaa70d8fd87eb99c2620ed5997defb2475
Basit5159 <basit5159@gmail.com>
2017-04-02 13:06:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,9 +19,9 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $event['source']['userId']
+				'text' => $text
 			];
-//$text
+//$event['source']['userId']
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
